45d1595ca6df3092024f1e4fb2db1cf67c2fc8d8
Mohamed Abdelhady <mohamedabdelhady9937@gmail.com>
2023-10-26 17:42:30 +0300

    problem 2 solution

--- A2_S8_20220277_2.cpp
+++ A2_S8_20220277_2.cpp
@@ -2,10 +2,49 @@
 
 using namespace std;
 
-map<string, string> inclusive{{"he", "he/she"}, {"him", "him/her"}, {"himself", "himself/herself"},
-                              {"male", "male/female"}};
+map<string, string> inclusive{{"he",      "he/she"},
+                              {"him",     "him/her"},
+                              {"himself", "himself/herself"}};
+
+string toLowerCase(string s) {
+    for (auto &i: s) {
+        if (i >= 'A' && i <= 'Z')i += 32;
+    }
+    return s;
+}
+
+void replace(string &word, bool &Upper) {
+    if (inclusive.count(toLowerCase(word)))word = inclusive[toLowerCase(word)];
+    if (Upper) {
+        word.front() -= 32;
+        auto it = word.find('/');
+        word[it + 1] -= 32;
+        Upper = false;
+    }
+}
+
+string replaceMasculinePronouns(string &sentence) {
+    string newSentence, word;
+    bool Upper;
+    if (sentence[0] >= 'A' && sentence[0] <= 'Z')Upper = true;
+    for (int i = 0; i < sentence.size(); ++i) {
+        if (sentence[i] == ' ' || sentence[i] == '.') {
+            replace(word, Upper);
+            newSentence += word;
+            newSentence.push_back(sentence[i]);
+            word = "";
+            if (sentence[i + 1] >= 'A' && sentence[i + 1] <= 'Z')Upper = true;
+        } else word.push_back(sentence[i]);
+    }
+    replace(word, Upper);
+    newSentence += word;
+    return newSentence;
+}
 
 int main() {
-
+    cout << "Input sentence to convert male speech to inclusive speech that includes both male and female\n>>";
+    string sentence;
+    getline(cin, sentence);
+    cout << replaceMasculinePronouns(sentence) << '\n';
     return 0;
 }
