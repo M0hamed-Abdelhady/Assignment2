dcebd7ce70e6af390748d3b2c81c1354e24ec117
Ahmed Osama <ahmedosamaft@gmail.com>
2023-10-26 20:02:07 +0300

    Add Task 2

---
+++ Task 2/BigReal.h
@@ -0,0 +1,16 @@
+#ifndef ASSIGNMENT2_BIGREAL_H
+#define ASSIGNMENT2_BIGREAL_H
+#include <string>
+
+namespace Task_2 {
+
+    class BigReal {
+    private:
+        bool isValidReal(std::string realNumber);
+
+
+    };
+
+}// namespace Task_2
+
+#endif//ASSIGNMENT2_BIGREAL_H
